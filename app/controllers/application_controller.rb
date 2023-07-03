1570d721f4d935c13a30a0d2d958ea704a34886f
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-03 19:22:03 +0200

    Fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,16 @@
 class ApplicationController < ActionController::Base
-    rescue_from CanCan::AccessDenied do |exception|
-      redirect_to root_url, alert: exception.message
-    end
-  
-    protect_from_forgery with: :exception
-  
-    before_action :update_allowed_parameters, if: :devise_controller?
-  
-    protected
-  
-    def update_allowed_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
-      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
-    end
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
   end
-  
+
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
+end
