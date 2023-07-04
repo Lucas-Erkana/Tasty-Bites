0d3a55a8742f7797a4d17c102096b469964f23b7
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-07-04 12:46:13 +0200

    Update recipe_controller.rb

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,19 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_recipe, only: %i[show edit update destroy]
 
   # GET /recipes or /recipes.json
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.where(user: current_user)
   end
 
   # GET /recipes/1 or /recipes/1.json
-  def show; end
+  def show
+    @recipe = Recipe.includes(:recipe_food).find(params[:id])
+    @ingredients = @recipe.recipe_food.where(recipe: @recipe)
+
+    redirect_to '/not_accessible' if (cannot? :manage, @recipe) && @recipe.public == false
+  end
 
   # GET /recipes/new
   def new
@@ -20,7 +26,7 @@ class RecipesController < ApplicationController
   # POST /recipes or /recipes.json
   def create
     @recipe = Recipe.new(recipe_params)
-
+    @recipe.user = current_user
     respond_to do |format|
       if @recipe.save
         format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully created.' }
@@ -34,19 +40,14 @@ class RecipesController < ApplicationController
 
   # PATCH/PUT /recipes/1 or /recipes/1.json
   def update
-    respond_to do |format|
-      if @recipe.update(recipe_params)
-        format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully updated.' }
-        format.json { render :show, status: :ok, location: @recipe }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @recipe.errors, status: :unprocessable_entity }
-      end
-    end
+    @recipe.public = !@recipe.public
+    @recipe.save
+    redirect_to recipe_url(@recipe), notice: 'Recipe was successfully updated.'
   end
 
   # DELETE /recipes/1 or /recipes/1.json
   def destroy
+    RecipeFood.destroy_by(recipe: @recipe)
     @recipe.destroy
 
     respond_to do |format|
@@ -59,11 +60,12 @@ class RecipesController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_recipe
-    @recipe = Recipe.find(params[:id])
+    @recipe = Recipe.find_by(id: params[:id])
+    redirect_to '/not_found' if @recipe.nil?
   end
 
   # Only allow a list of trusted parameters through.
   def recipe_params
-    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id)
   end
 end
