16ecedd102872515b398f342dce3c8c382f30c0b
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-07-04 15:53:33 +0200

    Add Ability model to manage user permissions.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
-class Recipe < ApplicationRecord
-  belongs_to :user
-  has_many :recipe_food
-
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
   validates :name, presence: true
-  validates :preparation_time, numericality: { only_integer: true, greater_or_equal_to: 0 }
-  validates :cooking_time, numericality: { only_integer: true, greater_or_equal_to: 0 }
-  validates :description, presence: true
-  validates :public, inclusion: { in: [true, false], message: 'should be a boolean value' }
 end
