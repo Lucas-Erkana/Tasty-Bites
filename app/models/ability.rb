bdd1e3436409a54966d887d267fbdb55a51ee0aa
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-07-04 16:05:45 +0200

    Fix linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,6 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-
     can :read, :all
     can :manage, Recipe, user_id: user.id
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   validates :name, presence: true
 end
