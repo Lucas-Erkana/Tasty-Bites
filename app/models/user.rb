1a72dedc5364e5985350fcfb89c5371c954d40e9
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-07-04 15:44:44 +0200

    “Create RecipeFood model with associations, validations, and additional methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_food
 
-
   validates :name, presence: true
   validates :preparation_time, numericality: { only_integer: true, greater_or_equal_to: 0 }
   validates :cooking_time, numericality: { only_integer: true, greater_or_equal_to: 0 }
