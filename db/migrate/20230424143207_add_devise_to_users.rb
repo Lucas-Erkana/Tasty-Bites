ec7fe8c9f8c427d5a2dc6d89a6fbd88267e402e0
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-05 19:16:46 +0200

    Spec setup

--- db/migrate/20230424143207_add_devise_to_users.rb
+++ db/migrate/20230424143207_add_devise_to_users.rb
@@ -8,8 +8,8 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      # t.string   :reset_password_token
+      # t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
@@ -38,7 +38,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
     end
 
     add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
